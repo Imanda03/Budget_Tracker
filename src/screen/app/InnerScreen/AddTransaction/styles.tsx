cb0a73e3d92de9e5d6868bca88cefacae812990c
Anish SHarma <anish.shara@gmail.com>
2024-12-26 12:36:08 +0545

    Initialize the transaction screen

---
+++ src/screen/app/InnerScreen/AddTransaction/styles.tsx
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native';
+import {useTheme} from '../../../../utils/colors';
+
+const createStyles = () => {
+  const {theme} = useTheme();
+  return StyleSheet.create({});
+};
